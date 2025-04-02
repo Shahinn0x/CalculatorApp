e049a8ae9bb434ac83714195dc4a87e652c1c8ae
shahin <shahin14623@gmail.com>
2025-04-02 23:33:24 +0530

    fisrt commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+}
+
+android {
+    namespace = "com.codeWithShahin.practice"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.codeWithShahin.practice"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+}
